0e4735e2364f7434e413354ed1bd3df7a4be74ed
Kevin Rattai <krattai@gmail.com>
2016-03-21 23:40:27 -0500

    Added a heartbeat scripts

---
+++ src/idetcore/hrtbt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# provides heartbeat
+#
+# Copyright (C) 2016 IHDN, Uvea I. S., Kevin Rattai
+#
+# This will send an message to announce alive and addresses
+
+i="0"
+
+while [ $i -lt 1 ]
+do
+hostn=$(cat /etc/hostname)
+ext_ip4=$(dig +short myip.opendns.com @resolver1.opendns.com)
+ext_ip6=$(curl icanhazip.com)
+# mosquitto_pub -d -t hello/world -m "$(date) : irot LdB, online. IP is $ext_ip" -h "uveais.ca"
+# mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn server IP $ext_ip is online." -h "2604:8800:100:19a::2"
+mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn IPv4 $ext_ip4 is online." -h "ihdn.ca"
+mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn IPv6 $ext_ip6 is online." -h "ihdn.ca"
+# i=$[$i+1]
+sleep 300
+done
+
