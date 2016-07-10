e864b1c50ca034c731148055fa6cbbb6584149f3
Kevin Rattai <krattai@gmail.com>
2016-07-10 15:38:14 -0500

    Added vpn ip pub to hrtbt.sh

--- src/idetcore/hrtbt.sh
+++ src/idetcore/hrtbt.sh
@@ -16,6 +16,10 @@ ext_ip6=$(curl icanhazip.com)
 # mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn server IP $ext_ip is online." -h "2604:8800:100:19a::2"
 mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn IPv4 $ext_ip4 is online." -h "ihdn.ca"
 mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn IPv6 $ext_ip6 is online." -h "ihdn.ca"
+
+IPt0=$(ip addr show tun0 | awk '/inet / {print $2}' | cut -d/ -f 1)
+mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn tun0 $IPt0 is online." -h "ihdn.ca"
+
 # i=$[$i+1]
 sleep 300
 done
