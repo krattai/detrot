fa70d1247b93fde6c0f6178ed6195e46d75fd45a
Kevin Rattai <krattai@gmail.com>
2016-07-10 18:26:55 -0500

    Changed pub channel for ctrlwtch.sh functions

--- src/idetcore/ctrlwtch.sh
+++ src/idetcore/ctrlwtch.sh
@@ -46,7 +46,7 @@ while [ ! -f "${HOME}/ctrl/reboot" ]; do
     if [ -f "${HOME}/ctrl/patch" ]; then
 
         hostn=$(cat /etc/hostname)
-        mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn patching." -h "ihdn.ca" &
+        mosquitto_pub -d -t ihdn/alladin/log -m "$(date) : $hostn patching." -h "ihdn.ca" &
 
         /run/shm/scripts/patch.sh &
         sudo chown pi:pi "${HOME}/ctrl/patch"
@@ -56,7 +56,7 @@ while [ ! -f "${HOME}/ctrl/reboot" ]; do
     # force patch rollback
     if [ -f "${HOME}/ctrl/rollback" ]; then
         hostn=$(cat /etc/hostname)
-        mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn rolling back patches." -h "ihdn.ca" &
+        mosquitto_pub -d -t ihdn/alladin/log -m "$(date) : $hostn rolling back patches." -h "ihdn.ca" &
         /run/shm/scripts/rollback.sh &
         sudo chown pi:pi "${HOME}/ctrl/rollback"
         rm "${HOME}/ctrl/rollback"
@@ -65,7 +65,7 @@ while [ ! -f "${HOME}/ctrl/reboot" ]; do
      # force system rebuild, bringing system to current
     if [ -f "${HOME}/ctrl/rebuild" ]; then
         hostn=$(cat /etc/hostname)
-        mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn system rebuilding." -h "ihdn.ca" &
+        mosquitto_pub -d -t ihdn/alladin/log -m "$(date) : $hostn system rebuilding." -h "ihdn.ca" &
         /run/shm/scripts/rebuild.sh &
         sudo chown pi:pi "${HOME}/ctrl/rebuild"
         rm "${HOME}/ctrl/rebuild"
@@ -74,7 +74,7 @@ while [ ! -f "${HOME}/ctrl/reboot" ]; do
    # change hostname
     if [ -f "${HOME}/ctrl/hostname" ]; then
         hostn=$(cat /etc/hostname)
-        mosquitto_pub -d -t ihdn/alive -m "$(date) : $hostn hostname changing." -h "ihdn.ca" &
+        mosquitto_pub -d -t ihdn/alladin/log -m "$(date) : $hostn hostname changing." -h "ihdn.ca" &
         mv "${HOME}/ctrl/hostname" "${HOME}/ctrl/newhost"
         /run/shm/scripts/chhostname.sh &
         hostn=$(cat /etc/hostname)
