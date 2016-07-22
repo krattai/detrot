8b80871372b828da7d160c5f6069a13e391a9456
Kevin Rattai <krattai@gmail.com>
2016-07-22 15:24:52 -0500

    Added call to start new IR detector

--- src/idetcore/det.sh
+++ src/idetcore/det.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #
+# Copyright (C) 2014 - 2016 IHDN, Uvea I. S., Kevin Rattai, Larry Schultz
 # 
 # Dec 2015 Larry
 # 
+# 2016-07-22
+# Kevin
+# Added call to start new IR detector
+#
 # Fix screen saver
 sudo setterm -blank 1
 # 
@@ -14,7 +19,15 @@ sudo setterm -blank 1
 
 # sudo /home/pi/bin/eye_2.sh &
 # sleep 2
-sudo /home/pi/bin/det_28.sh
+
+# start det_irk.sh
+sudo /home/pi/bin/det_irk.sh &
+
+# Wait 5 seconds to ensure IR det ready
+sleep 5
+
+# start det_28.sh
+sudo /home/pi/bin/det_28.sh &
 
 # sudo /home/pi/scripts/det_7.sh  
 exit 0
